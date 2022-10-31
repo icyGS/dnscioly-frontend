fa942515880a661b08ec929bf16de9833efcda35
icyGS <icyGS@users.noreply.github.com>
2022-10-31 01:15:14 +0000

    deploy: fb6570cdceb093d0923f662da63aba16db81bff7

--- assets/js/search.js
+++ assets/js/search.js
@@ -47,7 +47,7 @@ jtd.onReady = function(ready) {
 
 function initSearch() {
     var request = new XMLHttpRequest();
-    request.open('GET', '/dnscioly/assets/js/search-data.json', true);
+    request.open('GET', '/dnscioly-frontend/assets/js/search-data.json', true);
   
     request.onload = function(){
       if (request.status >= 200 && request.status < 400) {
